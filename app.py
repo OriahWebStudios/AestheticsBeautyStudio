fe79b4516175b8aaf76216244ed09ba36002a052
OriahWebStudios <oriahwebstudios@outlook.com>
2025-03-07 15:24:46 +0200

    Remove snippet

--- app.py
+++ app.py
@@ -45,10 +45,6 @@ class Users(UserMixin, db.Model):
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), unique=True, nullable=False) 
 
-with app.app_context(): 
-    db.create_all()
-
-
 
 @login_manager.user_loader
 def load_user(user_id):
